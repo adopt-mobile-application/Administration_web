823ab5d70914da0e496b5367103cbeda1bc11c71
Rajaa-Bouaziz <rajaebouaziz@gmail.com>
2021-02-04 22:53:57 +0100

    page login

--- src/main/java/com/web/administration/admin/AdminApplication.java
+++ src/main/java/com/web/administration/admin/AdminApplication.java
@@ -1,5 +1,4 @@
 package com.web.administration.admin;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +8,4 @@ public class AdminApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AdminApplication.class, args);
 	}
-
 }
